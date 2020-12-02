db8d1637859530f810307a64831aaa8e55fb4547
mathiyalagan <mathiyalagantnj@gmail.com>
2020-12-02 11:51:45 +0530

    created new branch

--- EFNgApp/EFNgApp/Controllers/EmployeeController.cs
+++ EFNgApp/EFNgApp/Controllers/EmployeeController.cs
@@ -6,7 +6,6 @@ using EFNgApp.Interfaces;
 using EFNgApp.Models;
 using Microsoft.AspNetCore.Mvc;
 
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace EFNgApp.Controllers
 {
